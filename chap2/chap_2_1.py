7126d9e6f5f098b057e0e8d47193a9d8ed369b22
LuWei <luwei@ykecf.cn>
2019-01-22 09:45:32 +0800

    index in string

--- chap2/chap_2_1.py
+++ chap2/chap_2_1.py
@@ -13,6 +13,18 @@ months = [
     'December'
 ]
 
-month = input('请输入需要查询的月份:\n')
-month_name = months[int(month) - 1]
-print(month_name + ' = ' + month + "月")
+endings = ['st', 'nd', 'rd'] + 17 * ['th'] \
+          + ['st', 'nd', 'rd'] + 7 * ['th'] \
+          + ['st']
+
+year = input('Year: ')
+month = input('Month(1-12): ')
+day = input('Day(1-31): ')
+
+month_number = int(month)
+day_number = int(day)
+
+month_name = months[month_number - 1]
+ordinal = day + endings[day_number - 1]
+
+print(month_name + ' ' + ordinal + ', ' + year)
