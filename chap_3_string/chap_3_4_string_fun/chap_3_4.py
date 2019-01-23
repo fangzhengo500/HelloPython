96ade9e99ecab3a93f6325302d4396b27f44b16c
LuWei <luwei@ykecf.cn>
2019-01-23 10:00:01 +0800

    3.4

---
+++ chap_3_string/chap_3_4_string_fun/chap_3_4.py
@@ -0,0 +1,24 @@
+from string import digits
+from string import ascii_letters
+from string import ascii_lowercase
+from string import ascii_uppercase
+from string import printable
+from string import punctuation
+
+# 包含数字0~9
+print(digits)
+
+# 所有ASCII字母的字符串
+print(ascii_letters)
+
+# 所有ASCII小写字母
+print(ascii_lowercase)
+
+# 所有ASCII大写字母
+print(ascii_uppercase)
+
+# 所有ASCII可打印字符
+print(printable)
+
+# 所有ASCII字符串
+print(punctuation)
