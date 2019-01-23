d6f33ef052faff6f87d600f382753a6b719257f6
LuWei <luwei@ykecf.cn>
2019-01-23 14:28:53 +0800

    string functions

--- chap_3_string/chap_3_4_string_fun/chap_3_4_4_lower.py
+++ chap_3_string/chap_3_4_string_fun/chap_3_4_4_lower.py
@@ -1,8 +1,12 @@
 print('Trondheim Hammer Dance'.lower())
 
+# 转小写
 name = 'Gumby'
 names = ['gumby', 'smith', 'jones']
 if name.lower() in names:
     print('Found it')
 else:
     print('Not Found')
+
+# 词首大写
+print('that\'s all folks'.title())
---
+++ chap_3_string/chap_3_4_string_fun/chap_3_4_5_replace.py
@@ -0,0 +1,5 @@
+print('This is a test'.replace('is', 'eez'))
+
+print('Any time, any where, any one'.replace('any', 'this'))
+
+# string.replace 替换会 替换string中所有可匹配的词
