6179e74fb2fd6c84ef64c016bdd34a160f12f862
LuWei <luwei@ykecf.cn>
2019-01-23 17:33:57 +0800

    python base

---
+++ chapter_5/chapter_5_1/chapter_5_1_2_import.py
@@ -0,0 +1,11 @@
+# 导入重命名
+
+# 模板导入方式
+# import somemoudule
+# from somemoudule import somefunction
+# from somemoudule import function1,function2
+# from somemoudule import *
+
+import math as foobar
+
+print(foobar.sqrt(4))
---
+++ chapter_5/chapter_5_2/chapter_5_2_1.py
@@ -0,0 +1,9 @@
+# 序列解包
+a = 1, 2, 3
+print(a)
+
+x, y, z = a
+print(x, y, z)
+
+x, y = y, x
+print(x, y, z)
