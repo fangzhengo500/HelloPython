02f57b7bebb90a2d38f6f1048f706f7d84d5af8b
LuWei <luwei@ykecf.cn>
2019-01-23 17:06:56 +0800

    print

---
+++ chapter_5/chapter_5_1/chapter_5_1_1_print.py
@@ -0,0 +1,9 @@
+# 打印多个表达式
+print('Age:', 42, 'phone is', 562.1)
+
+# 自定义分隔符
+print('I', 'wish', 'to', 'register', 'a', 'complanit', sep='_')
+
+# 自定义结束符（去掉换行符）
+print('Hello,',end='')
+print('Python!')
