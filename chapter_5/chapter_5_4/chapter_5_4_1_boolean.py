8e1cd136094acd19df15e2297bf2fed47c6d17eb
LuWei <luwei@ykecf.cn>
2019-01-24 14:02:44 +0800

    python

---
+++ chapter_5/chapter_5_4/chapter_5_4_1_boolean.py
@@ -0,0 +1,10 @@
+print(True)
+print(False)
+print('True == 1', True == 1)
+print('False == 0', False == 0)
+print(True + False + 66)
+
+print(bool('I think, therefore I am'))
+print(bool(''))
+print(bool(-1))
+print(bool(0))
---
+++ chapter_5/chapter_5_4/chapter_5_4_2_if.py
@@ -0,0 +1,5 @@
+name = input('What is your name?\n')
+if name.endswith('Gumby'):
+    print('Hello, Mr.Gumby')
+else:
+    print('Hello, stranger')
