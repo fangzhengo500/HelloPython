7061285358d2b39c500be7f779cf8a8288933bdf
LuWei <luwei@ykecf.cn>
2019-01-28 17:32:18 +0800

    abstract

---
+++ chapter_6_abstract/chapter_6_3_custom_function/chapter_6_3.py
@@ -0,0 +1,15 @@
+# 判断对象是否可以调用
+import math
+
+x = 1
+y = math.sqrt
+
+print("callable(x) :", callable(x))
+print("callable(y) :", callable(y))
+
+
+# 自定义函数
+def hello(name):
+    return 'Hello, '+name+"!"
+
+print(hello('Python'))
---
+++ chapter_6_abstract/chapter_6_3_custom_function/chapter_6_3_1_docstring.py
@@ -0,0 +1,9 @@
+# 函数文档
+def square(x):
+    'Calculates the square of the number x.'
+    return x * x;
+
+print(square.__doc__)
+print(print.__doc__)
+
+help(square)
